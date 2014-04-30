c414f60089a9f828dab22da673c5dbce627b41cd
RonaldBernal <ronald_antonio777@hotmail.com>
2014-04-29 21:26:27 -0500

    Se agregaron funciones para sacar sumatorias y se esta trabajando en la funcion para sacar la matriz a resolver

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import csv
+import math
 
 def readFile(filename = "datos.txt"):
 	#print()
@@ -22,13 +23,45 @@ def readFile(filename = "datos.txt"):
 			sys.exit('file {}, line {}: {}'.format(filename, reader.line_num, e))
 	return datos
 
-readFile()
-'''
+def calculaSumatorias(datos, orden = 3):
+	sumX = 0
+	sumY = 0
+	n = len(datos)
+	for i in range(n):
+		sumY += datos[i][1]
+
+	sumatoriasX = [0 for x in range(2*orden)]
+	sumatoriasXY = [0 for x in range(orden)]
+
+	
+	for i in range(2*orden):
+		for j in range(len(datos)):
+			sumatoriasX[i] += math.pow(datos[j][0], i+1)
+			if (i < orden):
+				sumatoriasXY[i] += datos[j][1] * math.pow(datos[j][0], i+1)
+
+	return sumatoriasX, sumatoriasXY
+
+def calculaEcuaciones(sumatoriasX, sumatoriasXY, n, orden = 3):
+	ecuaciones = [ [0 for x in range(orden + 2)] for x in range(orden + 1)]
+	
+	for fila in range(orden + 1):
+		for col in range(orden +2):
+			if(fila=0 and col=0):
+				ecuaciones[fila][col] = n
+			elif(j=orden+1):
+				ecuaciones[fila][col] = sumatoriasXY[]
+			else:
+				#ecuaciones[fila][col] = 
+
+
+	
+
 option = "";
 while (option != "x"):
 	print()
-	print("A) Interpolacion Exacta")
-	print("B) Interpolacion Aproximada")
+	print("A) Ajuste Polinomial Automatico")
+	print("B) Orden Polinomial Manual")
 	print("X) Salir")
 	print()
 
@@ -36,6 +69,13 @@ while (option != "x"):
 	if(option == "A" or option == "a"):
 		datos = readFile()
 		print()
-		valor = float(input("Por favor indique el valor a interpolar: "))
-		Lagrange(datos, valor)
-'''
+		sumatoriasX, sumatoriasXY = calculaSumatorias(datos)
+		calculaEcuaciones(sumatoriasX, sumatoriasXY, len(datos))
+		
+
+
+	if(option =="B" or option == "b"):
+		orden = int(input("Por favor indique el orden polinomial: "))
+		sumatoriasX, sumatoriasXY = calculaSumatorias(datos, orden)
+		calculaEcuaciones(sumatoriasX, sumatoriasXY, len(datos), orden)
+
